caa4dacccf0238c18fec84a28dbf7636030aa6bf
aleixfortm <aleix.fort45@gmail.com>
2023-07-14 01:55:23 +0200

    changes

--- MAPS/main.go
+++ MAPS/main.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+//
+
 func main() {
 	colors := map[string]string{
 		"red":   "#FF0000",
@@ -11,10 +13,10 @@ func main() {
 	colors2 := make(map[int]string)
 	colors2[1] = "#FFFFFF"
 	colors2[2] = "#000000" // Accessing a value in maps works differently than structs
-	// here we use square brackets instead
+	// Here we use square brackets instead
 
-	// deleting key-value pairs from a map
-	//delete(colors, "red")
+	// Deleting key-value pairs from a map
+	// delete(colors, "red")
 
 	printMap(colors)
 }
