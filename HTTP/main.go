368e033789cb4759a54a23564a3a0a84c0eb48db
aleixfortm <aleix.fort45@gmail.com>
2023-07-14 16:18:31 +0200

    changes

--- HTTP/main.go
+++ HTTP/main.go
@@ -7,11 +7,14 @@ import (
 )
 
 func main() {
-	resp, err := http.Get("http://google.com")
+	resp, err := http.Get("https://aleixfortm.pythonanywhere.com/posts")
 	if err != nil {
 		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
 
-	fmt.Println(resp)
+	bs := make([]byte, 99999)
+	resp.Body.Read(bs)
+	fmt.Println(string(bs))
+
 }
